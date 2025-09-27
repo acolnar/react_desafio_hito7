25fc91e8f8b02da168d8df45679e37ca63b031d2
Alex <alex.colihuinca@gmail.com>
2025-09-27 11:13:05 -0300

    Subiendo base del hito 4

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react_desafio_hito3/',
+  base: '/react_desafio_hito4/',
   plugins: [react()],
   build: {outDir: 'docs'}
 })
